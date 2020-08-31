9f0a54515edf5b3d6f6867fc8bab12ed89afcd87
harsh <harsh@192.168.0.109>
2020-08-30 22:41:42 -0400

    Minor Changes

--- src/InventoryAllocator.java
+++ src/InventoryAllocator.java
@@ -13,9 +13,11 @@ public class InventoryAllocator {
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 
 		InventoryAllocator t = new InventoryAllocator();
-		t.test1WhereallGood();
-		t.test2WhereallGood();
-		t.test3WhereallGood();
+		t.test1();
+		t.test2();
+		t.test3();
+		t.test4();
+		t.test5();
 	}
 
 	/**
@@ -31,9 +33,7 @@ public class InventoryAllocator {
 
 		ObjectMapper mapper = new ObjectMapper();
 		HashMap<String, Integer> input = mapper.readValue(inputCustomerRequest.getBytes(), HashMap.class);
-		System.out.println(input);
 		ArrayList<HashMap<String, Object>> inventory = mapper.readValue(storeInventory.getBytes(), ArrayList.class);
-		System.out.println(inventory);
 
 		ArrayList<HashMap<String, Object>> response = new ArrayList<>();
 
@@ -71,30 +71,46 @@ public class InventoryAllocator {
 
 	}
 
-	private void test1WhereallGood() throws JsonParseException, JsonMappingException, IOException {
+	private void test1() throws JsonParseException, JsonMappingException, IOException {
+		System.out.println("this is a positive case where input is  satisfing all the requirments And customer have multiple items ");
 		String inputCustomerRequest = "{\"apple\": 1, \"orange\":2,\"banana\":3}";
 		String storeInventory = "[{ \"name\": \"owd\", \"inventory\": { \"apple\": 10 ,\"orange\":2} }, { \"name\": \"dm\", \"inventory\": { \"apple\": 30, \"orange\": 10,\"banana\": 10 } }]";
 
 		this.mainFunction(inputCustomerRequest, storeInventory);
-
 	}
 
-	private void test2WhereallGood() throws JsonParseException, JsonMappingException, IOException {
-
-		String inputCustomerRequest = "{\"apple\": 3, \"orange\":2}";
-		String storeInventory = "[{ \"name\": \"owd\", \"inventory\": { \"apple\": 10 } }, { \"name\": \"GKP\", \"inventory\": { \"orange\": 10 } }]";
+	private void test2() throws JsonParseException, JsonMappingException, IOException {
+		System.out.println("this is a semi-positive case, customer did not get the full order just the partial order");
+		String inputCustomerRequest = "{\"apple\": 30, \"orange\":7}";
+		String storeInventory = "[{ \"name\": \"owd\", \"inventory\": { \"apple\": 10 } }, { \"name\": \"dm\", \"inventory\": { \"orange\": 10 } }]";
 
 		this.mainFunction(inputCustomerRequest, storeInventory);
 
 	}
 
-	private void test3WhereallGood() throws JsonParseException, JsonMappingException, IOException {
-
-		String inputCustomerRequest = "{\"apple\": 3, \"orange\":2}";
-		String storeInventory = "[{ \"name\": \"owd\", \"inventory\": { \"apple\": 10 } }, { \"name\": \"GKP\", \"inventory\": { \"orange\": 10 } }]";
+	private void test3() throws JsonParseException, JsonMappingException, IOException {
+		System.out.println("this is a positive case where we satisfing the requirment ");
+		String inputCustomerRequest = "{ \"orange\":2}";
+		String storeInventory = "[{ \"name\": \"owd\", \"inventory\": { \"apple\": 10 } }, { \"name\": \"dm\", \"inventory\": { \"orange\": 10 } }]";
 
 		this.mainFunction(inputCustomerRequest, storeInventory);
 
 	}
 
+	private void test4() throws JsonParseException, JsonMappingException, IOException {
+		System.out.println("there is no customer input and the API is invoked");
+		String inputCustomerRequest = "{ }";
+		String storeInventory = "[{ \"name\": \"owd\", \"inventory\": { \"apple\": 10 } }, { \"name\": \"dm\", \"inventory\": { \"orange\": 10 } }]";
+
+		this.mainFunction(inputCustomerRequest, storeInventory);
+
+	}
+	private void test5() throws JsonParseException, JsonMappingException, IOException {
+		System.out.println("the customer requested an item which was not in the storage ");
+		String inputCustomerRequest = "{ \"Pineapple\":10}";
+		String storeInventory = "[{ \"name\": \"owd\", \"inventory\": { \"apple\": 10 } }, { \"name\": \"dm\", \"inventory\": { \"orange\": 10 } }]";
+
+		this.mainFunction(inputCustomerRequest, storeInventory);
+
+	}
 }
